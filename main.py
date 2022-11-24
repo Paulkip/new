664d2ffd3a05f06e4090bfd39f0b2c9812337797
Paulkip <92159954+Paulkip@users.noreply.github.com>
2022-11-24 13:25:31 -0800

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import pytube
 
 app = Flask(__name__)
 
-
+path="C:\Users\Public\Downloads"
 @app.route('/')
 def index():
     url = 'https://www.youtube.com/watch?v=E_5d-jPeIVg'
     youtube = pytube.YouTube(url)
-    youtube.streams.filter(res="144p").first().download()
+    youtube.streams.filter(res="144p").first().download(path)
     return ("hello world")
 
 
