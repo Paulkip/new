d98b5d2e94b0590218f235d71bb335eb25ab629d
Paulkip <92159954+Paulkip@users.noreply.github.com>
2022-11-24 13:28:56 -0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pytube
 
 app = Flask(__name__)
 
-path="C:\Users\Public\Downloads"
+path="C:/Users/Public/Downloads"
 @app.route('/')
 def index():
     url = 'https://www.youtube.com/watch?v=E_5d-jPeIVg'
