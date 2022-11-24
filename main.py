c0baca800222d90c261e1b0110d239404418a758
Paulkip <92159954+Paulkip@users.noreply.github.com>
2022-11-24 13:20:14 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from flask import Flask, jsonify
 import os
+import pytube
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
+    url = 'https://www.youtube.com/watch?v=E_5d-jPeIVg'
+    youtube = pytube.YouTube(url)
+    youtube.streams.filter(res="144p").first().download()
     return ("hello world")
 
 
